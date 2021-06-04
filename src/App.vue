d39f1f748268a461c6e69f4b29887070b9cfb4f4
jacobhsu <jacob.hsu.tw@gmail.com>
2021-06-04 15:57:08 +0800

    vite to @vue/cli 4.5.10

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script>
 import HelloWorld from './components/HelloWorld.vue'
 
-export default defineComponent({
+export default {
   name: 'App',
   components: {
     HelloWorld
   }
-})
+}
 </script>
 
 <style>
